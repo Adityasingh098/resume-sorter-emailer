790b28d7d0cdca97c6b4086a0577390ca058186d
Aditya Singh <adityasingh917632@gmail.com>
2025-07-07 12:27:59 +0530

    Testing basic Streamlit UI

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Test App", layout="centered")
+
+st.title("✅ Streamlit App Loaded")
+st.write("If you're seeing this, everything is working fine.")
